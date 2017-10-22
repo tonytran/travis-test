822f32b318d07d992cad954e174fed11e48500da
tonytran <tony.tran@wne.edu>
2017-10-22 01:32:42 -0400

    add Pow_Imperative to math.go

--- mathutils/math.go
+++ mathutils/math.go
@@ -14,3 +14,19 @@ func Pow(x int, n int) (int, error) {
 		return x * y, err
 	}
 }
+
+func Pow_Imperative(x int, n int)(int, error) {
+    if n < 0 {
+        return 0, errors.New("bad input")
+    } else if n == 0 {
+        return 1, nil
+    } else {
+        result := 1
+        for n >= 1 {
+            result*=x
+            n-=1
+            
+        }
+        return result
+}
+	
