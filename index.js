f3516ecc7889b31b423e8245424fc7092c8cb4b3
Victor Sui <victorsui11@gmail.com>
2017-10-21 22:02:05 -0500

    reduce enthusiasm

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Travis!\n');
+  res.end('Hello Travis.\n');
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running...');
